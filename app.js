41b4cc38e5a0e71401c5162dafab29637b7ed294
Ray-Shakex <bshakex@gmail.com>
2021-10-12 15:09:06 +0200

    style.css

--- app.js
+++ app.js
@@ -13,18 +13,24 @@ for (var i=0; themeDots.length > i; i++){
     themeDots[i].addEventListener('click', function(){
         let mode = this.dataset.mode;
         console.log("clicked:", mode);
+        
         setTheme(mode);
+
     })
 }
 
 function setTheme(mode){
-    if(mode == 'light'){
-        document.getElementById('theme-style').href = 'css/style.css'
+    if(mode == 'style'){
+        document.getElementById('theme-style').href = 'css/style.css';
+        let toggle = document.getElementById('style-mode');
+        console.log(toggle);
+        // toggle.classList.toggle('active');
+        // toggle.classList.toggle(".active"); 
     }
-    if(mode == 'blue'){
+    if(mode == 'light'){
         document.getElementById('theme-style').href = 'css/light.css'
     }
-    if(mode == 'green'){
+    if(mode == 'dark'){
         document.getElementById('theme-style').href = 'css/dark.css'
     }
     localStorage.setItem('theme', mode)
